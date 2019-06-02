05c9c4deacb86d81cec68cf932294aea9799c8bf
pnm6054 <pnm6054@naver.com>
2019-06-03 02:23:47 +0900

    mp4 to csv format string

--- sketch.js
+++ sketch.js
@@ -9,29 +9,20 @@ PoseNet using p5.js
 === */
 /* eslint-disable */
 
+/* mp4 to csv format */
+
 // Grab elements, create settings, etc.
 var video = document.getElementById('video');
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var conf = document.getElementById('conf');
-var nose = document.getElementById('nose');
-var leye = document.getElementById('leye');
-var reye = document.getElementById('reye');
-var lear = document.getElementById('lear');
-var rear = document.getElementById('rear');
-var vidwidth, vidheight;
+var str = '';
+document.body.style = "white-space: pre;"
 // The detected positions will be inside an array
 let poses = [];
-
 // Create a webcam capture
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-    video.srcObject=stream;
-    video.play();
-    vidwidth = video.width, vidheight = video.height;
-  });
+if (navigator.mediaDevices) {
+  video.play();
 }
-
 // A function to draw the video and poses into the canvas.
 // This function is independent of the result of posenet
 // This way the video will not seem slow if poseNet 
@@ -41,7 +32,6 @@ function drawCameraIntoCanvas() {
   ctx.drawImage(video, 0, 0, 640, 480);
   // We can call both functions to draw all keypoints and the skeletons
   drawKeypoints();
-  //drawSkeleton();
   window.requestAnimationFrame(drawCameraIntoCanvas);
 }
 // Loop over the drawCameraIntoCanvas function
@@ -63,38 +53,23 @@ function modelReady() {
 // A function to draw ellipses over the detected keypoints
 function drawKeypoints()  {
   // Loop through all the poses detected
+  str_n = '';
   for (let i = 0; i < poses.length; i++) {
     // For each pose detected, loop through all the keypoints
-    for (let j = 0; j < poses[i].pose.keypoints.length; j++) {
+    str_n += poses[0].pose.score;
+    for (let j = 0; j < 11; j++) {
       let keypoint = poses[i].pose.keypoints[j];
-      conf.innerText = poses[0].pose.score;
       // Only draw an ellipse is the pose probability is bigger than 0.2
       if (keypoint.score > 0.2) {
+        str_n += ',' + keypoint.position.x + ',' + keypoint.position.y;
         ctx.beginPath();
         ctx.arc(keypoint.position.x, keypoint.position.y, 10, 0, 2 * Math.PI);
         ctx.stroke();
-        nose.innerText = poses[0].pose.keypoints[0].position.x / vidwidth + " " + poses[0].pose.keypoints[0].position.y / vidheight;
-        leye.innerText = poses[0].pose.keypoints[1].position.x / vidwidth + " " + poses[0].pose.keypoints[1].position.y / vidheight;
-        reye.innerText = poses[0].pose.keypoints[2].position.x / vidwidth + " " + poses[0].pose.keypoints[2].position.y / vidheight;
-        lear.innerText = poses[0].pose.keypoints[3].position.x / vidwidth + " " + poses[0].pose.keypoints[3].position.y / vidheight;
-        rear.innerText = poses[0].pose.keypoints[4].position.x / vidwidth + " " + poses[0].pose.keypoints[4].position.y / vidheight;
       }
     }
   }
-}
-
-// A function to draw the skeletons
-function drawSkeleton() {
-  // Loop through all the skeletons detected
-  for (let i = 0; i < poses.length; i++) {
-    // For every skeleton, loop through all body connections
-    for (let j = 0; j < poses[i].skeleton.length; j++) {
-      let partA = poses[i].skeleton[j][0];
-      let partB = poses[i].skeleton[j][1];
-      ctx.beginPath();
-      ctx.moveTo(partA.position.x, partA.position.y);
-      ctx.lineTo(partB.position.x, partB.position.y);
-      ctx.stroke();
-    }
+  if (str_n != str) {
+    document.body.appendChild(document.createTextNode(str_n+'\n'));
+    str = str_n;
   }
 }
