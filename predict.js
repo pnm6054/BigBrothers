f7f698deef0887540eb9b5b4bc19f483b267ae1b
pnm6054 <pnm6054@naver.com>
2019-06-04 00:21:51 +0900

    save and load model, weight initial interface of project

--- predict.js
+++ predict.js
@@ -2,21 +2,61 @@
  * Get the car data reduced to just the variables we are interested
  * and cleaned of missing data.
  */
+let data;
+let pred;
+function dist(ax, ay, bx, by) {
+  return Math.sqrt(Math.pow((ax - bx), 2) + Math.pow((ay - by), 2));
+}
 async function getData() {
-    const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');  
-    const carsData = await carsDataReq.json();  
-    const cleaned = carsData.map(car => ({
-      mpg: car.Miles_per_Gallon,
-      horsepower: car.Horsepower,
+    const DataReq = await fetch('data/sj.json');
+    const Data = await DataReq.json();  
+    const cleaned = Data.map(pose => ({
+      conf: pose.confidence,
+      left_eye: dist(pose.noseX, pose.noseY, pose.left_eyeX, pose.left_eyeY),
+      right_eye: dist(pose.noseX, pose.noseY, pose.right_eyeX, pose.right_eyeY),
+      left_ear: dist(pose.noseX, pose.noseY, pose.left_earX, pose.left_earY),
+      right_ear: dist(pose.noseX, pose.noseY, pose.right_earX, pose.right_earY),
+      left_shoulder: dist(pose.noseX, pose.noseY, pose.left_shouldX, pose.left_shouldY),
+      right_shoulder: dist(pose.noseX, pose.noseY, pose.right_shouldX, pose.right_shouldY),
+      left_elbow: dist(pose.noseX, pose.noseY, pose.left_elbowX, pose.left_elbowY),
+      right_elbow: dist(pose.noseX, pose.noseY, pose.right_elbowX, pose.right_elbowY),
+      left_wrist: dist(pose.noseX, pose.noseY, pose.left_wristX, pose.left_wristY),
+      right_wrist: dist(pose.noseX, pose.noseY, pose.right_wristX, pose.right_wristY),
+      focused: pose.focused,
     }))
-    .filter(car => (car.mpg != null && car.horsepower != null));
+    .filter(pose => (pose.conf != null));
     
     return cleaned;
   }
 
+async function getTestData() {
+  const DataReq = await fetch('data/cjtest.json');
+  const Data = await DataReq.json();  
+  const cleaned = Data.map(pose => ({
+    conf: pose.confidence,
+    left_eye: dist(pose.noseX, pose.noseY, pose.left_eyeX, pose.left_eyeY),
+    right_eye: dist(pose.noseX, pose.noseY, pose.right_eyeX, pose.right_eyeY),
+    left_ear: dist(pose.noseX, pose.noseY, pose.left_earX, pose.left_earY),
+    right_ear: dist(pose.noseX, pose.noseY, pose.right_earX, pose.right_earY),
+    left_shoulder: dist(pose.noseX, pose.noseY, pose.left_shouldX, pose.left_shouldY),
+    right_shoulder: dist(pose.noseX, pose.noseY, pose.right_shouldX, pose.right_shouldY),
+    left_elbow: dist(pose.noseX, pose.noseY, pose.left_elbowX, pose.left_elbowY),
+    right_elbow: dist(pose.noseX, pose.noseY, pose.right_elbowX, pose.right_elbowY),
+    left_wrist: dist(pose.noseX, pose.noseY, pose.left_wristX, pose.left_wristY),
+    right_wrist: dist(pose.noseX, pose.noseY, pose.right_wristX, pose.right_wristY),
+    focused: pose.focused,
+  }))
+  .filter(pose => (pose.conf != null));
+  
+  return cleaned;
+}
+
 async function run() {
   // Load and plot the original input data that we are going to train on.
-  const data = await getData();
+  data = await getData();
+  test = await getTestData();
+  console.log('data loaded');
+  /*
   const values = data.map(d => ({
     x: d.horsepower,
     y: d.mpg,
@@ -31,21 +71,26 @@ async function run() {
       height: 300
     }
   );
-
-  // More code will be added below
+  */
   
-    // Create the model
-    const model = createModel();  
-    tfvis.show.modelSummary({name: 'Model Summary'}, model);
+  // Create the model
+  const model = createModel();  
+  tfvis.show.modelSummary({name: 'Model Summary'}, model);
 
-    // Convert the data to a form we can use for training.
-    const tensorData = convertToTensor(data);
-    const {inputs, labels} = tensorData;
-        
-    // Train the model  
-    await trainModel(model, inputs, labels);
-    console.log('Done Training');
-    testModel(model, data, tensorData);
+  // Convert the data to a form we can use for training.
+  const tensorData = convertToTensor(data);
+  const {inputs, labels} = tensorData;
+
+  const testTensorData = convertToTensor(test);
+  const {testin, testla} = testTensorData;
+  console.log(testTensorData);
+
+  //const testTensorData = convertToTensor(test);
+  // Train the model  
+  await trainModel(model, inputs, labels);
+  console.log('Done Training');
+  await model.save('downloads://my-model');
+  testModel(model, testin, testTensorData);
 }
 
 document.addEventListener('DOMContentLoaded', run);
@@ -55,10 +100,12 @@ function createModel() {
     const model = tf.sequential(); 
     
     // Add a single hidden layer
-    model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
+    model.add(tf.layers.dense({inputShape: [11,], units: 16, useBias: true, activation: 'relu6'}));
     
+    model.add(tf.layers.dense({units: 16, useBias: true, activation: 'relu6'}));
+
     // Add an output layer
-    model.add(tf.layers.dense({units: 1, useBias: true}));
+    model.add(tf.layers.dense({units: 1, useBias: true, activation: 'sigmoid'}));
   
     return model;
   }
@@ -76,12 +123,11 @@ function convertToTensor(data) {
     return tf.tidy(() => {
       // Step 1. Shuffle the data    
       tf.util.shuffle(data);
-  
       // Step 2. Convert data to Tensor
-      const inputs = data.map(d => d.horsepower)
-      const labels = data.map(d => d.mpg);
+      const inputs = data.map(d => Object.values(d).slice(0,11));
+      const labels = data.map(d => Object.values(d).slice(11,12));
   
-      const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+      const inputTensor = tf.tensor2d(inputs, [inputs.length, 11]);
       const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
   
       //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
@@ -96,11 +142,13 @@ function convertToTensor(data) {
       return {
         inputs: normalizedInputs,
         labels: normalizedLabels,
+        //inputs: inputTensor,
+        //labels: labelTensor,
         // Return the min/max bounds so we can use them later.
-        inputMax,
-        inputMin,
-        labelMax,
-        labelMin,
+        //inputMax,
+        //inputMin,
+        //labelMax,
+        //labelMin,
       }
     });  
   }
@@ -108,12 +156,13 @@ async function trainModel(model, inputs, labels) {
     // Prepare the model for training.  
     model.compile({
         optimizer: tf.train.adam(),
-        loss: tf.losses.meanSquaredError,
+        loss: 'binaryCrossentropy',
+        //loss: 'tf.losses.meanSquaredError',
         metrics: ['mse'],
     });
 
-    const batchSize = 28;
-    const epochs = 50;
+    const batchSize = 20;
+    const epochs = 20;
 
     return await model.fit(inputs, labels, {
         batchSize,
@@ -128,11 +177,12 @@ async function trainModel(model, inputs, labels) {
 }
 
 function testModel(model, inputData, normalizationData) {
-    const {inputMax, inputMin, labelMin, labelMax} = normalizationData;  
+    //const {inputMax, inputMin, labelMin, labelMax} = normalizationData;  
     
     // Generate predictions for a uniform range of numbers between 0 and 1;
     // We un-normalize the data by doing the inverse of the min-max scaling 
     // that we did earlier.
+    /*
     const [xs, preds] = tf.tidy(() => {
       
       const xs = tf.linspace(0, 1, 100);      
@@ -149,8 +199,12 @@ function testModel(model, inputData, normalizationData) {
       // Un-normalize the data
       return [unNormXs.dataSync(), unNormPreds.dataSync()];
     });
-    
+    */
+    //pred = model.predict(inputData);
+    pred = model.predict(tf.randomNormal([20, 11]));
+    console.log(pred)
    
+    /*
     const predictedPoints = Array.from(xs).map((val, i) => {
       return {x: val, y: preds[i]}
     });
@@ -158,7 +212,6 @@ function testModel(model, inputData, normalizationData) {
     const originalPoints = inputData.map(d => ({
       x: d.horsepower, y: d.mpg,
     }));
-    
     
     tfvis.render.scatterplot(
       {name: 'Model Predictions vs Original Data'}, 
@@ -169,4 +222,5 @@ function testModel(model, inputData, normalizationData) {
         height: 300
       }
     );
+    */
   }
